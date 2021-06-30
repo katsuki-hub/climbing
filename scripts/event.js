ba1ff70157c06e36fceb456452ec953009888469
katsuki-hub <44katsu26@gmail.com>
2021-06-30 19:53:05 +0900

    アップデート~ver1.3~

--- scripts/event.js
+++ scripts/event.js
@@ -1,7 +1,7 @@
 var events = [
   { 'Date': new Date(2021, 5, 29), "Title": "mono 19:00 ~" },
   { 'Date': new Date(2021, 5, 28), "Title": "6月4週~道場課題の写真更新~"},
-  { 'Date': new Date(2021, 5, 30), "Title": "健康診断ファイト"},
+  { 'Date': new Date(2021, 6, 6), "Title": "ザウォール 10：00 ~"},
 ];
 
 var settings = {};
