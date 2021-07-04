6606e15ce04afa5b3a3923127ae22f2046a6073b
katsuki-hub <44katsu26@gmail.com>
2021-07-04 22:03:38 +0900

    イベントゴール

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -9,7 +9,7 @@ var countdown = function (due) {
   return count;
 }
 
-var goal = new Date(2021, 6, 4, 12);
+var goal = new Date(2021, 6, 6, 19);
 
 var recalc = function () {
   var counter = countdown(goal);
