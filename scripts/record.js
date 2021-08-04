3be9aa4109ab3a87c2aecc9f07acfb90bfc8e608
katsuki-hub <44katsu26@gmail.com>
2021-08-05 00:36:36 +0900

    女子予選リザルト

--- scripts/record.js
+++ scripts/record.js
@@ -53,3 +53,60 @@ for (var i = 0; i < mq8.length; i++) {
   td.textContent = mq8[i];
   document.getElementById("mq8").appendChild(td);
 }
+
+
+var wq1 = ["1", "ヤンヤ・ガルンブレト", "スロベニア", "14.00", "1.00", "4.00", "56.00"];
+for (var i = 0; i < wq1.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq1[i];
+  document.getElementById("wq1").appendChild(td);
+}
+
+var wq2 = ["2", "徐　采鉉", "韓国", "17.00", "5.00", "1.00", "85.00"];
+for (var i = 0; i < wq2.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq2[i];
+  document.getElementById("wq2").appendChild(td);
+}
+
+var wq3 = ["3", "野中　生萌", "日本", "4.00", "8.00", "3.00", "96.00"];
+for (var i = 0; i < wq3.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq3[i];
+  document.getElementById("wq3").appendChild(td);
+}
+
+var wq4 = ["4", "野口　啓代", "日本", "9.00", "3.00", "6.00", "162.00"];
+for (var i = 0; i < wq4.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq4[i];
+  document.getElementById("wq4").appendChild(td);
+}
+
+var wq5 = ["5", "ブルック・ラバトゥ", "アメリカ", "12.00", "2.00", "8.00", "192.00"];
+for (var i = 0; i < wq5.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq5[i];
+  document.getElementById("wq5").appendChild(td);
+}
+
+var wq6 = ["6", "ジェシカ・ピルツ", "オーストリア", "11.00", "9.00", "2.00", "198.00"];
+for (var i = 0; i < wq6.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq6[i];
+  document.getElementById("wq6").appendChild(td);
+}
+
+var wq7 = ["7", "アレクサンドラ・ミロスワフ", "ポーランド", "1.00", "20.00", "19.00", "380.00"];
+for (var i = 0; i < wq7.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq7[i];
+  document.getElementById("wq7").appendChild(td);
+}
+
+var wq8 = ["8", "アヌク・ジョベール", "フランス", "2.00", "13.00", "15.00", "390.00"];
+for (var i = 0; i < wq8.length; i++) {
+  var td = document.createElement("td");
+  td.textContent = wq8[i];
+  document.getElementById("wq8").appendChild(td);
+}
