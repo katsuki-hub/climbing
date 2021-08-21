ce5070569715ffc1038617bf9fe63412897b4dc0
katsuki-hub <44katsu26@gmail.com>
2021-08-21 17:58:43 +0900

    barehands

---
+++ scripts/images.js
@@ -0,0 +1,32 @@
+/*=====================
+にゅ～ん
+=====================*/
+function smoothAnime() {
+    $(".smoothTrigger").each(function () {
+      var elemPos = $(this).offset().top - 50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass("smooth");
+      } else {
+        $(this).removeClass("smooth");
+      }
+    });
+  }
+  $(window).scroll(function () {
+    smoothAnime();
+  });
+
+var thumbs = document.querySelectorAll(".thumb");
+for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].onclick = function () {
+        document.getElementById("bigimg").src = this.dataset.image;
+    };
+}
+
+var thumbs = document.querySelectorAll(".thumb2");
+for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].onclick = function () {
+        document.getElementById("bigimg2").src = this.dataset.image;
+    };
+}
